065424e95f6c542854035748f0c866a325893223
Alexandre Boyer <ajb@zerospam.ca>
2012-12-26 18:03:31 -0500

    Added functions and switches for easier use.

--- xenserver/prepare-xenserver.sh
+++ xenserver/prepare-xenserver.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
-## Install generic stuff
-yum -y --enablerepo=base install vim-enhanced emacs
+OFFICE=`getent hosts host.example.com | cut -d' ' -f1`
+VPN="192.168.0.0/16"
+IPTCONF="/etc/sysconfig/iptables"
 
-## Simple but usable vimrc
-cat > /root/.vimrc <<EOF
+fixall () {
+    ## Fix yum: add fc6 extras
+    if [ ! -f /etc/yum.repos.d/fc6extras.repo ]; then
+	cat > /etc/yum.repos.d/fc6extras.repo <<EOF
+# [fc6-extras]
+name=Fedora Core 6 Extras
+mirrorlist=http://mirrors.fedoraproject.org/mirrorlist?repo=extras-6&arch=$basearch
+enabled=0
+gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-fedora-extras
+gpgcheck=0
+EOF
+    fi
+
+    ## Install generic stuff
+    yum --enablerepo=base install vim-enhanced emacs
+    yum --enablerepo=fc6-extras install ulogd
+
+    ## fix ulog daemon
+    sed -i 's/^loglevel=.\+$/loglevel=3/g' /etc/ulogd.conf
+    chkconfig ulogd on
+    service ulogd start
+
+    ## Simple but usable vimrc
+    cat > /root/.vimrc <<EOF
 syntax on
 autoindent
 se ts=4
@@ -12,23 +35,23 @@ se expandtab
 se shiftwidth=4
 EOF
 
-## Always use the pretty vi
-if [ -e /usr/bin/vim ]; then
-    rm /bin/vi
-    ln -s /usr/bin/vim /bin/vi
-fi
+    ## Always use the pretty vi
+    if [ -e /usr/bin/vim ]; then
+	rm /bin/vi
+	ln -s /usr/bin/vim /bin/vi
+    fi
 
-## Now, always use emacs because vi sucks
-cat > /usr/bin/em <<EOF
+    ## Now, always use emacs because vi sucks
+    cat > /usr/bin/em <<EOF
 #!/bin/sh
 
 exec emacs -nw "\$@"
 EOF
 
-chmod 0700 /usr/bin/em
+    chmod 0700 /usr/bin/em
 
-# Good defaults for emacs ;)
-cat > /root/.emacs <<EOF
+    # Good defaults for emacs ;)
+    cat > /root/.emacs <<EOF
 (global-font-lock-mode t)
 (setq inhibit-splash-screen t)
 (setq-default c-basic-offset 4)
@@ -111,7 +134,7 @@ cat > /root/.emacs <<EOF
           (delete-file tmp-file)))))
 EOF
 
-cat > /root/.emacs-custom.el << EOF
+    cat > /root/.emacs-custom.el << EOF
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -136,22 +159,27 @@ cat > /root/.emacs-custom.el << EOF
  '(cursor ((t (:background "#ffffff")))))
 EOF
 
-## secure the webserver
-mv /opt/xensource/www/XenCenter.msi /opt/xensource/www/XenCenter.msi.disabled
-mv /opt/xensource/www/XenCenter.iso /opt/xensource/www/XenCenter.iso.disabled
-cat > /opt/xensource/www/Citrix-index.html <<EOF
+    ## secure the webserver
+    if [ mv /opt/xensource/www/XenCenter.msi /opt/xensource/www/XenCenter.msi.disabled ]; then
+	echo -e "\nMoved /opt/xensource/www/XenCenter.msi"
+    fi
+    if [ mv /opt/xensource/www/XenCenter.iso /opt/xensource/www/XenCenter.iso.disabled ]; then
+	echo -e "\nMoved /opt/xensource/www/XenCenter.iso"
+    fi
+    cat > /opt/xensource/www/Citrix-index.html <<EOF
 <html>
   <title>XenServer 2.1.0-haha.com</title>
+  <body>Go away.</body>
 </html>
 EOF
 
-## Prepare /root/.ssh/authorized_keys
-cat > /root/.ssh/authorized_keys <<EOF
-# Add your SSH keys here.
+    ## Prepare /root/.ssh/authorized_keys
+    cat > /root/.ssh/authorized_keys <<EOF
+# Paste your keys here
 EOF
 
-## Prepare sshd_config
-cat > /etc/ssh/sshd_config <<EOF
+    ## Prepare sshd_config
+    cat > /etc/ssh/sshd_config <<EOF
 Protocol 2
 SyslogFacility AUTHPRIV
 PermitRootLogin without-password
@@ -167,14 +195,14 @@ X11Forwarding yes
 Subsystem   sftp    /usr/libexec/openssh/sftp-server
 EOF
 
-# Restart sshd
-echo -e "\n\tRestarting ssh service:\n"
-service sshd restart
+    # Restart sshd
+    echo -e "\n\tRestarting ssh service:\n"
+    service sshd restart
 
-## Correct iptables
-sed -i 's/IPTABLES_MODULES="ip_conntrack_netbios_ns"/IPTABLES_MODULES=""/g' /etc/sysconfig/iptables-config
+    ## Correct iptables
+    sed -i 's/^IPTABLES_MODULES=".\+$/IPTABLES_MODULES=""/g' /etc/sysconfig/iptables-config
 
-cat > /etc/sysconfig/iptables <<EOF
+    cat > /etc/sysconfig/iptables <<EOF
 # rules as per `basename $0`
 # rolled on: `date`
 *filter
@@ -182,33 +210,123 @@ cat > /etc/sysconfig/iptables <<EOF
 :FORWARD ACCEPT [0:0]
 :OUTPUT ACCEPT [0:0]
 :RH-Firewall-1-INPUT - [0:0]
+:HTTPS-FOR-XENCENTER - [0:0]
+:LOGNDROP - [0:0]
 -A INPUT -j RH-Firewall-1-INPUT
+-A INPUT -j HTTPS-FOR-XENCENTER
+-A INPUT -j LOGNDROP
 -A FORWARD -j RH-Firewall-1-INPUT
+# loopback is the queen
 -A RH-Firewall-1-INPUT -i lo -j ACCEPT
--A RH-Firewall-1-INPUT -p icmp --icmp-type any -j ACCEPT
--A RH-Firewall-1-INPUT -p 50 -j ACCEPT
--A RH-Firewall-1-INPUT -p 51 -j ACCEPT
-# DNS Multicast
--A RH-Firewall-1-INPUT -p udp --dport 5353 -d 224.0.0.251 -j ACCEPT
-## IPP and CUPS
-# -A RH-Firewall-1-INPUT -p udp -m udp --dport 631 -j ACCEPT
-# -A RH-Firewall-1-INPUT -p tcp -m tcp --dport 631 -j ACCEPT
 # Established
 -A RH-Firewall-1-INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
-## ha-cluster
+# allow ping
+-A RH-Firewall-1-INPUT -p icmp --icmp-type any -j ACCEPT
+# # 50 (ESP) & 51 (AH): IPSec traffic (NOT NEEDED)
+# -A RH-Firewall-1-INPUT -p 50 -j ACCEPT
+# -A RH-Firewall-1-INPUT -p 51 -j ACCEPT
+# # Dns Multicast (NOT NEEDED)
+# -A RH-Firewall-1-INPUT -p udp --dport 5353 -d 224.0.0.251 -j ACCEPT
+# # Ipp and CUPS (NOT NEDDED)
+# -A RH-Firewall-1-INPUT -p udp -m udp --dport 631 -j ACCEPT
+# -A RH-Firewall-1-INPUT -p tcp -m tcp --dport 631 -j ACCEPT
+# # ha-cluster (NOT NEEDED yet)
 # -A RH-Firewall-1-INPUT -m state --state NEW -m udp -p udp --dport 694 -j ACCEPT
 # nagios ntp monitor
 -A RH-Firewall-1-INPUT -m state --state NEW -m udp -p udp --dport 123 -j ACCEPT
 # SSH
 -A RH-Firewall-1-INPUT -m state --state NEW -m tcp -p tcp --dport 22 -j ACCEPT
-# https is needed for XenCenter
--A RH-Firewall-1-INPUT -m state --state NEW -m tcp -p tcp --dport 443 -j ACCEPT
-# The end.
--A RH-Firewall-1-INPUT -j REJECT --reject-with icmp-host-prohibited
+## https is needed for XenCenter
+-A HTTPS-FOR-XENCENTER -m state --state NEW -m tcp -p tcp -s $OFFICE --dport 443 -j ACCEPT
+-A HTTPS-FOR-XENCENTER -m state --state NEW -m tcp -p tcp -s $VPN --dport 443 -j ACCEPT
+# Log and Drop
+-A LOGNDROP -p tcp -j ULOG --ulog-prefix "Denied TCP: "
+-A LOGNDROP -p udp -j ULOG --ulog-prefix "Denied UDP: "
+-A LOGNDROP -p icmp -j ULOG --ulog-prefix "Denied ICMP: "
+-A LOGNDROP -j DROP
 COMMIT
 EOF
 
-# Restart iptables
-echo -e "\n\tRestarting iptables:\n"
-service iptables restart
-service iptables status
+}
+
+iptablesrestart () {
+    echo -e "\n\tRestarting iptables:\n"
+    service iptables restart
+    service iptables status
+}
+
+manageon () {
+    echo -e "\nMode manage ON (opening port 443)\n"
+    sed -i '/INPUT -j HTTPS/ s/^# //' $IPTCONF
+}
+
+manageoff () {
+    echo -e "\nMode manage OFF (closing port 443)\n"
+    sed -i '/INPUT -j HTTPS/ s/^/# /' $IPTCONF
+}
+
+usage () {
+    cat <<EOF
+USAGE:
+
+$(basename $0) [options]
+
+Options:
+
+  -f, --fixall   : fixes the XenServer pristine install to fit our needs.
+                   Should be run only once after install and after each upgrade.
+
+  -o, --on       : turns on HTTPS (needed for XenCenter access)
+  -O, --off      : turns off HTTPS
+
+  -h, --help     : this help message
+
+EOF
+}
+
+## Argument parsing.
+if [ "$#" -eq 0 ]; then   # Script needs at least one command-line argument.
+    echo -e "\nWe need at least one argument.\n"
+    usage
+    exit 1
+fi
+
+## options may be followed by one colon to indicate they have a required argument
+OPTS=`getopt -o fhoO -l fixall,help,on,off -- "$@"`
+
+if [ $? != 0 ]; then
+    exit 1
+fi
+
+eval set -- "$OPTS"
+
+while true; do
+    case "$1" in
+	-f|--fixall)
+	    fixall
+	    iptablesrestart
+	    shift
+	    ;;
+	-o|--on)
+	    manageon
+	    iptablesrestart
+	    shift
+	    ;;
+	-O|--off)
+	    manageoff
+	    iptablesrestart
+	    shift
+	    ;;
+        -h|--help)
+            usage
+            shift
+            ;;
+        \?)
+            echo "Invalid option: $OPTARG"
+            ;;
+        :)
+            echo "Option $OPTARG requires an argument."
+            ;;
+        --) shift; break;;
+    esac
+done
