3f945559594d93b7e825c408fc320153a6dd9047
Alexandre Boyer <biggnou@gmail.com>
2015-08-17 10:37:25 -0400

    capture
    adding options (iface and filters) to tcpdump

--- capture/spynet.py
+++ capture/spynet.py
@@ -7,20 +7,32 @@
 # As seen on stackoverflow and else...
 ##-------------------
 
+from optparse import OptionParser
 import subprocess
 import sys
 import os
 
-def main():
-    check = os.getuid()
-    if check != 0:
-        print('This program requires root privileges. Exiting.')
-        sys.exit()
-    
-    p = subprocess.Popen(('tcpdump', '-l'), stdout=subprocess.PIPE)
+def tcpdump(iface, filters):
+    p = subprocess.Popen(['tcpdump', '-l', '-i', iface, filters], stdout=subprocess.PIPE)
     for row in iter(p.stdout.readline, b''):
         print row.rstrip()   # process here
 
+def main():
+    check = os.getuid()
+    if check != 0:  # check if user is root, since we're running tcpdump
+        print('This program requires root privileges. Exiting.')
+        sys.exit()
+
+    parser = OptionParser()
+    parser.add_option("-i", "--iface", dest="iface", default="eth0", help="Which interface? [default: %default]")
+    parser.add_option("-f", "--filters", dest="filters", default="tcp port 80 or 443", help="Specify filter capture [default: %default]")
+
+    (options, args) = parser.parse_args()
+    
+    iface = options.iface
+    filters = options.filters
+
+    tcpdump(iface, filters)
 
 
 if __name__ == '__main__':
